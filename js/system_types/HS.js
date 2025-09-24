eab4f730877de9dd822c4d1ac58e23da6242338e
Ted Malliaris <99051629+malliaris@users.noreply.github.com>
2025-09-24 19:56:13 -0400

    added radius R UICI button to HS; about to dive into adding IC UICI funtionality involving particle densities/radii/masses/positions/velocities/etc

--- js/system_types/HS.js
+++ js/system_types/HS.js
@@ -386,6 +386,7 @@ class Params_HS extends Params {
     static kT0;
     static UINI_v_pist;  // = new UINI_float(this, "UI_P_SM_HS_v_pist", true);  assignment occurs in UserInterface(); see discussion there
     static UICI_rho;  // = new UICI(this, "UI_P_SM_HS_rho", false);  assignment occurs in UserInterface(); see discussion there
+    static UICI_R;  // = new UICI(this, "UI_P_SM_HS_R", false);  assignment occurs in UserInterface(); see discussion there
 
     static R = 0.005;//0.001;
     static single_m_val_not_dist;
